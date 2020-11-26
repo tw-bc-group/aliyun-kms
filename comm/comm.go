b9c99bba772d279c1b1940dc8d0809a35654607e
Matrix <113445886@qq.com>
2020-11-26 21:22:56 +0800

    chore: hint error when needed env not found

--- comm/comm.go
+++ comm/comm.go
@@ -1,14 +1,22 @@
 package comm
 
 import (
+	"errors"
 	"os"
 
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/kms"
 )
 
 func CreateKmsClient() (*kms.Client, error) {
-	client, err := kms.NewClientWithAccessKey(os.Getenv("ALIBABA_CLOUD_REGION"),
-		os.Getenv("ALIBABA_CLOUD_ACCESS_KEY_ID"), os.Getenv("ALIBABA_CLOUD_ACCESS_KEY_SECRET"))
+	region := os.Getenv("ALIBABA_CLOUD_REGION")
+	accessKeyID := os.Getenv("ALIBABA_CLOUD_ACCESS_KEY_ID")
+	accessKeySecret := os.Getenv("ALIBABA_CLOUD_ACCESS_KEY_SECRET")
+
+	if len(region) == 0 || len(accessKeyID) == 0 || len(accessKeySecret) == 0 {
+		return nil, errors.New("ALIBABA_CLOUD_REGION, ALIBABA_CLOUD_ACCESS_KEY_SECRET, ALIBABA_CLOUD_ACCESS_KEY_SECRET must be set")
+	}
+
+	client, err := kms.NewClientWithAccessKey(region, accessKeyID, accessKeySecret)
 	if err != nil {
 		return nil, err
 	}
